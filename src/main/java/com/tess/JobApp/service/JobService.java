7bb8cd9f88b564f614dc6d3744c757ef76266cc1
tess <tesfa.py@gmail.com>
2026-02-25 19:20:59 -0500

    simplified job board for crud operations

---
+++ src/main/java/com/tess/JobApp/service/JobService.java
@@ -0,0 +1,31 @@
+package com.tess.JobApp.service;
+
+import com.tess.JobApp.model.JobPost;
+import com.tess.JobApp.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class JobService {
+    @Autowired
+    public JobRepo repo;
+
+
+    // method to add a jobPost
+    public void addJob(JobPost jobPost) {
+        repo.addJob(jobPost);
+
+    }
+
+
+    //method to return all JobPosts
+    public List<JobPost> getAllJobs() {
+        return repo.getAllJobs();
+
+
+    }
+
+
+}
